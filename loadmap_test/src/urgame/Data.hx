9981289556a85412dcd5d4176f6e458c77138248
Chelsea Davis <chelseadavis@hotmail.com>
2016-05-24 12:35:36 -0700

    Need asset loading in flambe

---
+++ loadmap_test/src/urgame/Data.hx
@@ -0,0 +1,4 @@
+package urgame;
+
+// private typedef Init = haxe.macro.MacroType < [cdb.Module.build("data.cdb")] > ;
+private typedef Init = haxe.macro.MacroType < [cdb.Module.build("../assets/bootstrap/data.cdb")] > ;
